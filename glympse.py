880a607233fb6d6d04e6f1fc2b3bd6d3104ecb6b
Chris Lacina <clacina@mindspring.com>
2016-04-12 14:45:21 -0700

    Sockets and DB

--- glympse.py
+++ glympse.py
@@ -1,6 +1,41 @@
 import tornado.web, tornado.ioloop
 from tornado import gen
 import motor
+import logging
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import string
+import random
+
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+clients = dict()
+
+
+def random_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
+
+
+db = motor.motor_tornado.MotorClient(
+    "mongodb://clacina:ptYpRKAqNvK7@ds023000.mlab.com:23000/glympse_web_socket-clacina"),
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/connect", ChatSocketHandler),
+        ],
+        settings = dict(
+            cookie_secret=random_generator(),
+            xsrf_cookies=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 
 class NewMessageHandler(tornado.web.RequestHandler):
@@ -44,17 +79,83 @@ class MessagesHandler(tornado.web.RequestHandler):
         self.write('</ul>')
         self.finish()
 
+    @tornado.web.asynchronous
+    @gen.coroutine
+    def connect(self):
+        self.write("hello")
+        self.finish()
+
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    waiters = set()
+    cache = []
+    cache_size = 200
+
+    def get_compression_options(self):
+        # Non-None enables compression with default options.
+        return {}
+
+    def open(self):
+        print("Web socket opened")
+        self.id = random_generator()
+        self.stream.set_nodelay(True)
+        clients[self.id] = {"id": self.id, "object": self}
+        ChatSocketHandler.waiters.add(self)
+        self.write_message("hello")
+
+    def on_close(self):
+        print("Web socket closed")
+        ChatSocketHandler.waiters.remove(self)
+
+    def on_message(self, message):
+        logging.info("got message %r", message)
+        msg = message.split()
+        if len(msg) < 2:
+            self.write_error("Invalid Command")
+        else:
+            if msg[0].lower() == 'get':
+                print("Get command")
+            elif msg[0].lower() == 'set':
+                print("Set command")
+            else:
+                output = "Unknown Command " + msg[0] + "}"
+                self.write_message(output)
+
+    def check_origin(self, origin):
+        return True
+
+    def select_subprotocol(self, subprotocols):
+        for l in subprotocols:
+            print(l)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello")
+        self.finish()
+        # self.render("index.html", messages=ChatSocketHandler.cache)
+
+
+class ConnectionsHandler(tornado.web.RequestHandler):
+    def get(self):
+        output = str(len(clients))
+        self.write("Connections : " + output)
+        self.finish()
+
 
 db = motor.motor_tornado.MotorClient("mongodb://clacina:ptYpRKAqNvK7@ds023000.mlab.com:23000/glympse_web_socket-clacina")
 
+
 application = tornado.web.Application(
     [
-        (r'/compose', NewMessageHandler),
-        (r'/', MessagesHandler)
+        (r"/", MainHandler),
+        (r"/connect", ChatSocketHandler),
+        (r"/connections", ConnectionsHandler),
     ],
     db=db
 )
 
-print('Listening on http://localhost:8888')
-application.listen(8888)
+tornado.options.parse_command_line()
+print('Listening on port ' + str(options.port))
+application.listen(options.port)
 tornado.ioloop.IOLoop.instance().start()
